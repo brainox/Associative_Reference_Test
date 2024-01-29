59398599a0a574576e6a93878ebf4faf19df66e2
Obinna <macmartins081@gmail.com>
2024-01-29 13:12:55 +0400

    Refactored and documented code to be easily readable.

--- Associative_Reference_Test/ViewController.swift
+++ Associative_Reference_Test/ViewController.swift
@@ -8,39 +8,64 @@
 import UIKit
 
 
-// Static variable to serve as the key for association
-private var key: UInt8 = 0
-
-class ViewController: UIViewController, UIAlertViewDelegate {
+class ViewController: UIViewController {
     
-    let alert = UIAlertController(title: "Dummy title", message: "Dummy message", preferredStyle: .alert)
+    // MARK: - Enums
+    
+    // Static variable to serve as the key for association
+    enum AssociatedKey {
+        static var key: Int = 0
+    }
+    
+    // MARK: - Properties
+    
+    private var button: UIButton = {
+        let button = UIButton()
+        button.setTitle("Tap me", for: .normal)
+        button.backgroundColor = .blue
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
+    // MARK: - View Lifecycles
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+        button.addTarget(self, action: #selector(onButtonTapped), for: .touchUpInside)
     }
+}
 
-    @IBAction func onAlertTapped(_ sender: UIButton) {
-        let payload = UIViewController()
+// MARK: - Private Methods
 
+extension ViewController {
+    
+    func setupUI() {
+        view.addSubview(button)
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+    
+    @objc func onButtonTapped() {
+        // create the alert and the payload
+        let alert = UIAlertController(title: "Title", message: "Example message", preferredStyle: .alert)
+        let payload = "Example message"
         // Associate the payload with the alert using the objc_setAssociatedObject function
-        objc_setAssociatedObject(alert, &key, payload, .OBJC_ASSOCIATION_RETAIN)
-
+        objc_setAssociatedObject(self, &AssociatedKey.key, payload, .OBJC_ASSOCIATION_RETAIN)
         // Add an OK button to the alert
         let okAction = UIAlertAction(title: "OK", style: .default) { [weak self] _ in
             self?.handleAlertDismissed()
         }
         alert.addAction(okAction)
-
         // Present the alert
         present(alert, animated: true, completion: nil)
     }
-
-    private func handleAlertDismissed() {
+    
+    func handleAlertDismissed() {
         // Retrieve the associated payload using objc_getAssociatedObject
-        if let payload = objc_getAssociatedObject(alert, &key) {
+        if let payload = objc_getAssociatedObject(self, &AssociatedKey.key) {
             // Log the payload
-            print("Payload was: \(payload)")
+            print("Payload is: \(payload)")
         }
     }
 }
